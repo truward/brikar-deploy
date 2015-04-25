14fe00175ac858cec50f0f9fafbc9da0b3f3a0e7
Alexander Shabanov <avshabanov@gmail.com>
2015-04-25 00:20:39 -0700

    Adding setup-layout and aws-deploy

---
+++ src/setup-layout.sh
@@ -0,0 +1,53 @@
+#!/bin/sh
+#
+# Server script for deploying applications with embedded servlet container.
+# 2015 Alexander Shabanov
+################################################################
+
+# Exit on error
+set -e
+
+#
+# Check input properties
+#
+
+if [ -z "$SERVICE_NAME" ]; then
+    echo "ERROR: Please, set up SERVICE_NAME environment variable before running this script"
+    exit -1
+fi
+
+if [ -z "$SRC_JAR" ]; then
+    echo "ERROR: Please, set up SRC_JAR (path to source app jar) variable before running this script"
+    exit -1
+fi
+
+if [ -z "$SRC_PROPS" ]; then
+    echo "ERROR: Please, set up SRC_PROPS (path to source app properties) variable before running this script"
+    exit -1
+fi
+
+#
+# Check for existence of source files
+#
+
+if [ ! -f $SRC_JAR ]; then
+    echo "ERROR: No jar file at $SRC_JAR"
+    exit -1
+fi
+
+if [ ! -f $SRC_PROPS ]; then
+    echo "ERROR: No property file at $SRC_PROPS"
+    exit -1
+fi
+
+#
+# Set up directory
+#
+
+BASE_DIR=/usr/local/$SERVICE_NAME
+
+mkdir -p $BASE_DIR/var/log
+
+cp $SRC_JAR $BASE_DIR/app.jar
+cp $SRC_PROPS $BASE_DIR/var/app.properties
+

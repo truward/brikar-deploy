536e51af43ce4555e9880ccb28505268f4009a89
Alexander Shabanov <avshabanov@gmail.com>
2017-02-05 17:08:53 -0800

    Adding manual setup script

--- src/fabric/skeleton/healthcheck.sh
+++ src/fabric/skeleton/healthcheck.sh
@@ -1,7 +1,7 @@
 # The output of healthcheck script should be exactly 'OK' to consider health check to be successful.
 
 # Uncomment (provide valid username:password and port number):
-# curl -s -u testonly:test -X POST http://127.0.0.1:8080/rest/health
+# curl -s -u testonly:test -X POST http://127.0.0.1:8080/api/health
 
 # Dummy no-op healthcheck
 echo 'OK'
